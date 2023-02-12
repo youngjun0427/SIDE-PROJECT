987d4455e0358c2d45113bce47c13aac509521f8
youngjun.choi <dlfgudwns12@gmail.com>
2023-02-12 17:44:59 +0900

    feat: 질문 슬라이드 기능 추가

--- my-app/src/components/Home/Home.jsx
+++ my-app/src/components/Home/Home.jsx
@@ -1,15 +1,34 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <>
-      <TestLogo src="images/test-logo.png" alt="로고" />
+      <LogoImg src="img/test-logo.png" alt="로고" />
+      <Link to="/Options">
+        <StartBtn>START</StartBtn>
+      </Link>
     </>
   );
 };
 
 export default Home;
 
-const TestLogo = styled.img`
-  justify-content: center;
+const LogoImg = styled.img`
+  width: 335px;
+  height: 160px;
+  margin: 0 auto;
+  margin-top: 259px;
+`;
+
+const StartBtn = styled.button`
+  width: 319px;
+  height: 64px;
+  margin-top: 32px;
+  color: #0094ff;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 36px;
+  background-color: whitesmoke;
+  border-radius: 16px;
 `;
--- my-app/src/components/Options/Options.jsx
+++ my-app/src/components/Options/Options.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { createRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './option.module.css';
+import Questions from '../../common/api/questionsApi.json';
 
 const Options = () => {
+  const [loading, setLoading] = useState(false);
+  const [num, setNum] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const slideRef = createRef(null);
+  const TOTAL_SLIDES = 12;
+  const history = useNavigate();
+  const [mbti, setMbti] = useState([]);
+
+  const slideFirst = () => {
+    setMbti(mbti + Questions[num].answer[0].type);
+    setNum(num + 1);
+    setCurrentSlide(currentSlide + 1);
+    slideRef.current.style.transform += 'translateX(-100vw)';
+  };
+
+  const slideSecond = () => {
+    setMbti(mbti + Questions[num].answers[1].type);
+    setNum(num + 1);
+    setCurrentSlide(currentSlide + 1);
+    slideRef.current.style.transform += 'translateX(-100vw)';
+  };
+
   return (
     <>
-      <h1>질문페이지 입니다.</h1>
+      <section>
+        {!loading && (
+          <>
+            <div className={styles.slider} ref={slideRef}>
+              {Questions.map(item => {
+                return (
+                  <div className={styles.content} key={item.id}>
+                    <div>
+                      <div>
+                        <span>{currentSlide}</span>
+                        <span>/{TOTAL_SLIDES}</span>
+                      </div>
+                      <h1>{item.Question}</h1>
+                    </div>
+                    <article>
+                      <button onClick={slideFirst}>
+                        {item.answers[0].content}
+                      </button>
+                      <button onClick={slideSecond}>
+                        {item.answers[1].content}
+                      </button>
+                    </article>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+        {loading && (
+          <div>
+            <img src="" alt="" />
+            <div></div>
+          </div>
+        )}
+      </section>
     </>
   );
 };
