79cba475ffdd85525f908415edd006de17aa20dd
youngjun.choi <dlfgudwns12@gmail.com>
2023-02-24 05:17:42 +0900

    docs: json 불러오기 성공 및 스타일링

--- my-app/src/components/Result/Result.jsx
+++ my-app/src/components/Result/Result.jsx
@@ -37,10 +37,10 @@ const Result = () => {
     return () => document.body.removeChild(script);
   }, []);
 
+  console.log(nation.features[0]);
   if (!nation) {
     return <div>존재하지 않는 결과입니다.</div>;
   }
-
   const copyAlert = () => {
     alert('링크 복사완료!');
   };
@@ -52,21 +52,21 @@ const Result = () => {
           <LogoImg src='../img/test-logo.png' alt='로고 이미지' />
         </Link>
       </>
+      <h1>{nation.subject}</h1>
+      <ImgBox>
+        <img src={nation.img} alt='img' />
+      </ImgBox>
       <ResultBox>
-        <h1>{nation.subject}</h1>
-        <ImgBox>
-          <img src={nation.img} alt='img' />
-        </ImgBox>
-        <span>{nation.features[0]}</span>
-        <span>{nation.features[1]}</span>
+        <span>{nation.features[0].des}</span>
+        <span>{nation.features[1].des}</span>
         <ul>
           {nation.description.map((item, index) => {
             return <li key={index}>{item.des}</li>;
           })}
         </ul>
       </ResultBox>
-      <div>
-        <h2>{nation.name}와 유형별 궁합</h2>
+      <DuoBox>
+        <h2>{nation.name}의 유형별 궁합</h2>
         <div>
           <Link to={`${/result/}${nation.duo[0].subhead}`}>
             <img src={nation.duo[0].img} alt='mbti캐릭터' />
@@ -87,7 +87,7 @@ const Result = () => {
             <p>{nation.counter[0].des}</p>
           </div>
         </div>
-      </div>
+      </DuoBox>
       <div>
         <button onClick={shareToKakaotalk}>카톡공유</button>
       </div>
@@ -109,6 +109,13 @@ const ResultSection = styled.section`
   background-color: var(--result-back-color);
   overflow: hidden;
 
+  & h1 {
+    font-weight: 400;
+    font-size: var(--fs-mx);
+    line-height: 120%;
+    color: var(--result-name-color);
+    margin-bottom: 2.4rem;
+  }
   /* ::-webkit-scrollbar {
     width: 0px;
     height: 0px;
@@ -124,14 +131,10 @@ const LogoImg = styled.img`
 `;
 
 const ResultBox = styled.div`
-  & h1 {
-    font-weight: 400;
-    font-size: var(--fs-mx);
-    line-height: 120%;
-    color: var(--result-name-color);
-    margin-bottom: 2.4rem;
-  }
-
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-bottom: 4.8rem;
   & span,
   ul {
     font-weight: 400;
@@ -139,6 +142,14 @@ const ResultBox = styled.div`
     line-height: 141.2%;
     color: var(--text-color);
     margin: 0 3rem;
+  }
+  & ul {
+    margin-top: 1rem;
+  }
+
+  & li {
+    list-style: circle;
+    padding: 0.5rem;
   }
 `;
 
@@ -155,5 +166,14 @@ const ImgBox = styled.div`
     width: 25rem;
     height: 25rem;
     margin: 0 auto;
+  }
+`;
+
+const DuoBox = styled.div`
+  & h2 {
+    font-weight: 400;
+    font-size: var(--fs-lg);
+    line-height: 141.2%;
+    color: var(--sub-text-color);
   }
 `;
